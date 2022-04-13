69379396b0506279ca811f4c674a18a5f963ceb6
Ljcruise <laciejcruise@gmail.com>
2022-04-13 13:24:01 -0500

    Pasted in code from other project

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+This script includes multiple functions to read an input file that outputs
+both valid and invalid data files
+"""
+
+__author__ = 'Lacie Cruise, Kyla Phillips'
+__version__ = '1.0'
+__copyright__ = 'Coding Competition'
+__github__ = 'https://github.com/Ljcruise/Coding_Comp.git'
+
+import csv
+import re
+
+id = 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def data_input(contact_info):
+    with open('input_file.csv', 'r') as f1, open('input_file2.csv', 'r') as f2:
+        writer = csv.writer(f1, f2, delimiter='|')
+        writer.writerows(contact_info)
 
 
-# Press the green button in the gutter to run the script.
+def add_contact_info(contact_info):
+    global id
+    id += 1
+    pattern = '(\d\d\d)(\d\d\d)(\d\d\d\d)'
+    new_pattern = r'\1.\2.\3'
+
+    #full_name = last_name.title() + ' , ' + first_name.title()
+
+    phone_num = input('Please enter your phone number: ')
+    new_phone_num = re.sub(pattern, new_pattern, phone_num)
+
+    data_input(contact_info)
+    read_file(contact_info)
+
+
+def read_file(contact_info):
+    with open('input_file.csv', 'r') as f1, open('input_file2.csv', 'r') as f2:
+        reader = csv.reader(f1, delimiter='|')
+        reader = csv.reader(f2)
+
+        for row in reader:
+            try:
+                id, full_name, email, phone_num = row
+
+            except:
+                print("Not enough information")
+
+            invalid_data = ''
+
+            try:
+                last_name, first_name = full_name.split(', ')
+            except:
+                invalid_data += 'N'
+
+            email_pattern = '[a-zA-Z0-9]+@[a-zA-Z]+\.(wsc)'
+
+            try:
+                (re.search(email_pattern, email))
+            except:
+                invalid_data += 'E'
+
+
+
+        return contact_info
+
+
+def main():
+    """
+    The main function
+    Args:
+        no value
+    Returns:
+        no value
+    """
+    contact_info = []
+    read_file(contact_info)
+    # add_contact_info(contact_info)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
